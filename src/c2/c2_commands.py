34bfc8b66a5b5701836135b22492fb272d2119c9
Host-Info.net <50676042+aerobotpro@users.noreply.github.com>
2020-03-20 23:15:50 -0700

    Update c2_commands.py

--- src/c2/c2_commands.py
+++ src/c2/c2_commands.py
@@ -3,7 +3,9 @@ def main():
     while True:     
         cmd = input(strings.hlp)
         try: open("null", "w+").write(cmd)
-        except: pass    
+        except Exception as d:
+            print(str(d))
+            pass
         sleep(3)
 class strings:
     hlp = """
